e990eb07b8a5ecf2c032082637477392f6a3444e
Kai Curry <kai@cape.io>
2016-03-09 10:33:36 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+QUIZ
+=====================
+
+## Content & Settings
+https://airtable.com/tblNuWBszb4Uiz5b1/viw1gT0pLbHMbSvlf
+
+## Code folks do this
+
+```
+git clone git@github.com:cape-io/med-synergies.git
+cd med-synergies
+npm install
+npm start
+open http://localhost:3000
+```
+
+## License
+
+?
---
+++ webpack.config.dev.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/js/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  module: {
+    loaders: [ {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: path.join(__dirname, 'src'),
+      loaders: ['babel?cacheDirectory']
+    } ],
+  },
+}
