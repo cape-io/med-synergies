fe2226e08eefdfe6081cea1e7539c06b22e8e84b
Kristian Bjornard <kb@ookb.co>
2016-03-16 10:36:34 -0500

    light blue leader stripe should go all the way across now

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,21 @@ import React, { PropTypes } from 'react'
 function Header({ logo, title, intro, lead }) {
   return (
     <header>
-      <div className="bar" style={{ height: 150 }}>
-        <a href={logo.href} title={logo.alt}>
-          <img src={logo.src} className="logo" alt={logo.alt} />
-        </a>
-        <h1 style={{ textTransform: 'uppercase' }}>{title}</h1>
+      <div className="container">
+        <div className="bar" style={{ height: 150 }}>
+          <a href={logo.href} title={logo.alt}>
+            <img src={logo.src} className="logo" alt={logo.alt} />
+          </a>
+          <h1 style={{ textTransform: 'uppercase' }}>{title}</h1>
+        </div>
       </div>
-      <div className="leader" style={{ background: '#00aeef' }}>
+    </header>
+    <div className="leader" style={{ background: '#00aeef' }}>
+      <div className="container">
         <h3 className="intro">{intro}</h3>
         <p style={{ marginTop: 20 }}>{lead}</p>
       </div>
-    </header>
+    </div>
   )
 }
 
